dadac8c6e98bae5b6aaba569c2f6bbf0ba98bbf2
finkkk <flx2217183968@gmail.com>
2026-09-11 00:06:38 +0800

    feat: 接入artalk系统

---
+++ docs/.vitepress/theme/Artalk.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { nextTick, onBeforeUnmount, onMounted, watch } from 'vue'
+import { useRoute } from 'vitepress'
+import 'artalk/Artalk.css'
+
+const route = useRoute()
+
+let artalkInstance: any = null
+
+async function mountArtalk() {
+  await nextTick()
+
+  const { default: Artalk } = await import('artalk')
+
+  artalkInstance?.destroy?.()
+
+  artalkInstance = Artalk.init({
+    el: '#Comments',
+    pageKey: window.location.pathname,
+    pageTitle: document.title,
+    server: 'https://comment.finkkk.cn',
+    site: 'Fink Framework 文档'
+  })
+}
+
+onMounted(mountArtalk)
+
+watch(() => route.path, mountArtalk)
+
+onBeforeUnmount(() => {
+  artalkInstance?.destroy?.()
+})
+</script>
+
+<template>
+  <div id="Comments"></div>
+</template>
---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import Artalk from './Artalk.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <Artalk />
+    </template>
+  </Layout>
+</template>
